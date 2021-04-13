c019e35f2ce13ca9f5cdbbef6fa685a0a0858451
Fahd <fahd31256@gmail.com>
2021-04-13 12:16:43 +0000

    added leaderboard sorting

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 let names = ["Fahd", "Amine", "Omar", "Mehdi", "Samar", "Yahia", "Sarah", "Axed", "Imane", "Jihwan"];
 let usernames = ["9JlZKTcvMO_XU2IBV-uvTyg65cN7Oghn0BrLP2JCS-SdQiq9", "kmIMD61kpFj__vdcmkFmWRTO3Y4iTcdo8v9RM8wT5r3oarmV", "sZSQ7WVlb8PjS1DVSLp8-qaQ15b2-yjWPrGWXJcRSzEBmPF4", "OYzdKBrBZlppkK0z7QAPUHV7Bz1znrLhD2k_Im4eTYFNdFuB", "GQG72tn5Ap1mrhn-LOqj6LyE56iCbIMeji9qTDCxqJ_Z2SUb", "001gvTdfRTfrToRzGEk-sr4M1p1u70A4cTR1zhTIwNn8QdVD", "FI5IP0jcSLuwHxaSZH-dJSNRLC2m65VurjA-LB57Pg8W1-wU", "3D_wRaLbvZS19lw8GD38C-X2VzGPnU8TaWWJmkWlNPlPaNaQ", "kpDMq3qrUAZkPy3IVd-s2urPQyKsIHZot_MF8qeQJznatFr7", "w6-mWOTSRvKTQyKyDVefKcyZgEQEiaGYIGDkBNRhWpu9d2OS"];
+let leaderboardArray = [];
 
 //Main Script
 const app = express();
@@ -25,22 +26,100 @@ async function main(){
 
      let leaderboard = "";
      while(true){
-          //Creating the ranks
           leaderboard = "";
-          leaderboard += "The month of April just started!! Good Luck on your ranked games!! \n Lets announce the Leaderboards!!\n\n";
+          leaderboard += "Happy Ramadan!! Good Luck on your ranked games!! \nLet's announce the Leaderboards!!\n\n";
+
+          // Adding ranks to leaderboard
           for(let i = 0; i < names.length; i++){
                let player = await getRank(usernames[i]);
-               leaderboard += (i + 1) + ") " + names[i] + ": " + player.tier + " " + player.rank + " " + player.lp + " LP";
-               leaderboard += "\n";
+               let mmr = rankToMMR(player.tier, player.rank, player.lp);
+               let name = names[i];
+               leaderboardArray.push({name, player, mmr});
                sleep(1000);
           }
 
+          //Sorting the array
+          leaderboardArray.sort(compare);
+
+          for(let i = 0; i < leaderboardArray.length; i++){
+               let player = leaderboardArray[i].player;
+               leaderboard += (i + 1) + ") " + leaderboardArray[i].name + " " + player.tier + " " + player.rank + " " + player.lp + " LP" + "\n";
+          }
+
+          //Adding date and time to leaderboard
+          leaderboard += "\n";
+
+          let date = new Date();
+          let day = date.getDate();
+          let month = date.getMonth();
+          let year = date.getFullYear();
+
+          let hours = date.getHours();
+          let minutes = date.getMinutes();
+
+          leaderboard += "Next Update: " + day + "/" + month + "/" + year + " " + hours + ":" + (minutes+5) + " UTC";
+
+
           //Updating the discord leaderboard message
           await message.edit(leaderboard);
-          console.log("Updated Leaderboard");
           sleep(300000);
      }
 
+}
+
+function compare(a, b){
+     return b.mmr - a.mmr;
+}
+
+function rankToMMR(tier, rank, lp){
+     let number = 0;
+     switch(tier){
+          case "challenger":
+               number += 90000;
+          break;
+          case "grandmaster":
+               number += 80000;
+          break;
+          case "grandmaster":
+               number += 70000;
+          break;
+          case "master":
+               number += 60000;
+          break;
+          case "diamond":
+               number += 50000;
+          break;
+          case "platinum":
+               number += 40000;
+          break;
+          case "gold":
+               number += 30000;
+          break;
+          case "silver":
+               number += 20000;
+          break;
+          case "bronze":
+               number += 10000;
+          break;
+     }
+     switch(rank){
+          case "I":
+               number += 4000;
+          break;
+          case "II":
+               number += 3000;
+          break;
+          case "III":
+               number += 2000;
+          break;
+          case "IV":
+               number += 1000;
+          break;
+     }
+
+     number += parseInt(lp);
+
+     return number;
 }
 
 async function getRank(puuid){
