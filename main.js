93a080666fd54c32fe361d3efb2df2def49c5d33
fqhd <fahd31256@gmail.com>
2021-06-26 22:27:09 +0100

    Integrated _update message with application api key

--- main.js
+++ main.js
@@ -5,44 +5,31 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 // Global Variables
-let RIOT_KEY;
 let leaderboardMessage;
 let logMessage;
-let names = ["Fahd", "Amine", "Omar", "Mehdi", "Samar", "Yahia", "Sarah", "Axed", "Imane", "Jihwan"];
-let usernames = ["9JlZKTcvMO_XU2IBV-uvTyg65cN7Oghn0BrLP2JCS-SdQiq9", "kmIMD61kpFj__vdcmkFmWRTO3Y4iTcdo8v9RM8wT5r3oarmV", "sZSQ7WVlb8PjS1DVSLp8-qaQ15b2-yjWPrGWXJcRSzEBmPF4", "OYzdKBrBZlppkK0z7QAPUHV7Bz1znrLhD2k_Im4eTYFNdFuB", "GQG72tn5Ap1mrhn-LOqj6LyE56iCbIMeji9qTDCxqJ_Z2SUb", "001gvTdfRTfrToRzGEk-sr4M1p1u70A4cTR1zhTIwNn8QdVD", "FI5IP0jcSLuwHxaSZH-dJSNRLC2m65VurjA-LB57Pg8W1-wU", "3D_wRaLbvZS19lw8GD38C-X2VzGPnU8TaWWJmkWlNPlPaNaQ", "kpDMq3qrUAZkPy3IVd-s2urPQyKsIHZot_MF8qeQJznatFr7", "w6-mWOTSRvKTQyKyDVefKcyZgEQEiaGYIGDkBNRhWpu9d2OS"];
+const names = ["Fahd", "Amine", "Omar", "Mehdi", "Samar", "Yahia", "Sarah", "Axed", "Imane", "Jihwan"];
+const usernames = ["PANV_xgn3lYQXsNSdCXViBhtIlMvzjvJnku-jdLHmCuFj0mF", "hDrk5vOdOW9AoTzs6WOvlb5Orctn_dMtsfgvzYPqxdtsJJmL", "o60Asiz2h1YejtyMF1SIg-QAz0RQxvNltcmGmRZcbMfrtDQW", "FoEoLD2s3QuHi7iggV_oSGxelhdgi7lzj-dgiIOKfamk0lBQ", "EXpLqDc2TG9hYALDdbPzBkVsr7y54UJPMeoahW7x3H6Bms1C", "fIrlYQ9O0dE9uAtpRRMBx2Z66bhE82FAhgGvjjx-ZxQGYihW", "i_WpCTFvx1feyR29DcD13Fl2b2bvffLpb8bI1-eurOLxLFiC", "5ZxZUR3iPXMjbhjlNSY2d5ixKYYtFJ19Q89YDafTyIPQ0sJK", "Uszb-967hvrhwyGzGe_tnsbh6STIDGn-Bi-X1dm_DfaRROE8", "Dw5v-dla666TOJ8PX8oRGJ9nEOWaaopjsb9eO4_H8fM893Vu"];
 let leaderboardArray = [];
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+const RIOT_KEY = process.env.RIOT_KEY;
 
 // Main Script
-client.login(process.env.DISCORD_TOKEN);
-client.on("ready", async () => {
-	await init();
-	console.log("Bot Started!");
-});
-client.on("message", messageEvent);
+client.login(DISCORD_TOKEN);
+client.on("ready", init);
+client.on("message", messageEntered);
 
-// Functions
-async function messageEvent(msg) {
-	if(msg.content.substring(0, 4) === "KEY="){
-		let providedKey = msg.content.substring(4);
-		let validity = await checkApiKey(providedKey);
-		if(validity == true){
-			// API Key is valid
-			RIOT_KEY = providedKey;
-			msg.channel.send("Successfully Updated API Key");
-		} else {
-			// API Key is not valid
-			msg.channel.send("Invalid API Key Provided");
+async function messageEntered(message){
+	if(message.channel.id == "831148754181816351"){
+		// Process the message
+		if(message.content == "_stop"){
+			console.log("Stopping bot...");
+			await message.delete();
+			client.destroy();
+		}else if(message.content == "_update"){
+			await message.delete();
+			updateLeaderboard();
 		}
-		
-	} else if (msg.content === "_update") {
-		console.log("Updating Leaderboard");
-		msg.delete().catch(err => console.log(err));
-		updateLeaderboard();
-	} else if (msg.content === "_stop"){
-		console.log("Stopping Bot...");
-		client.destroy();
 	}
-
 }
 
 async function init(){
@@ -53,12 +40,13 @@ async function init(){
 		leaderboardMessage = await channel.messages.fetch("831189816040357898");
 		logMessage = await channel.messages.fetch("852929017945522258");
 	}catch(err){
-		console.log(err);
+		console.log("Failed to initialize variables");
 	}
+	console.log("Bot started!");
 }
 
 async function updateLeaderboard() {
-	await logMessage.edit("Updating Leaderboard...").catch("Failed to update log message");
+	await logMessage.edit("Updating Leaderboard...");
 
 	leaderboard = "";
 	leaderboardArray = [];
@@ -68,9 +56,8 @@ async function updateLeaderboard() {
 	for(let i = 0; i < names.length; i++){
 		let player = await getRank(usernames[i]);
 		if(player == null){
-			await logMessage.edit("Failed to update leaderboard").catch("Failed to update log message");
-			console.log("Failed to update leaderboard");
-			return; // There was an error in the getRank() function, so we exit the function
+			await logMessage.edit("Failed to get rank of player: " + names[i]);
+			return;
 		}
 		let mmr = rankToMMR(player.tier, player.rank, player.lp);
 		let name = names[i];
@@ -93,23 +80,8 @@ async function updateLeaderboard() {
 	leaderboard += "Last Updated: " + Date();
 
 	// Updating the discord leaderboard message
-	await leaderboardMessage.edit(leaderboard).catch("Failed to update leaderboard message");
-	await logMessage.edit("Leaderboard Updated!").catch("Failed to update log message");
-}
-	
-
-async function checkApiKey(key){
-	try {
-		let response = await fetch("https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/" + usernames[0] + "?api_key=" + key);
-		if(response.status == 200){
-			return true;
-		}else{
-			return false;
-		}
-	}catch(err){
-		console.log(err);
-		return null;
-	}
+	await leaderboardMessage.edit(leaderboard);
+	await logMessage.edit("Leaderboard Updated!");
 }
 
 function compare(a, b){
@@ -167,36 +139,26 @@ function rankToMMR(tier, rank, lp){
 	return number;
 }
 
-
-
 async function getRank(puuid){
+	let region = (puuid == "Uszb-967hvrhwyGzGe_tnsbh6STIDGn-Bi-X1dm_DfaRROE8") ? "na1" : "euw1";
+
 	try {
-		if (puuid == "kpDMq3qrUAZkPy3IVd-s2urPQyKsIHZot_MF8qeQJznatFr7") {
-			let response = await fetch("https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/" + puuid + "?api_key=" + RIOT_KEY);
-			let data = await response.json();
-			for (let i = 0; i < data.length; i++) {
-				if (data[i].queueType == "RANKED_SOLO_5x5") {
-					let tier = data[i].tier.toLowerCase();
-					let rank = data[i].rank;
-					let lp = data[i].leaguePoints;
-					return { tier, rank, lp };
-				}
-			}
-		} else {
-			let response = await fetch("https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/" + puuid + "?api_key=" + RIOT_KEY);
-			let data = await response.json();
-			for (let i = 0; i < data.length; i++) {
-				if (data[i].queueType == "RANKED_SOLO_5x5") {
-					let tier = data[i].tier.toLowerCase();
-					let rank = data[i].rank;
-					let lp = data[i].leaguePoints;
-					return { tier, rank, lp };
-				}
-			}
-		}
-	}catch(err){
+		var response = await fetch("https://" + region + ".api.riotgames.com/lol/league/v4/entries/by-summoner/" + puuid + "?api_key=" + RIOT_KEY);
+		var data = await response.json();
+	} catch(err){
 		console.log(err);
+	}
+	if(response.status != 200){
+		console.log("Reponse failed with status code: " + response.status);
 		return null;
+	}
+	for (let i = 0; i < data.length; i++) {
+		if (data[i].queueType == "RANKED_SOLO_5x5") {
+			let tier = data[i].tier.toLowerCase();
+			let rank = data[i].rank;
+			let lp = data[i].leaguePoints;
+			return { tier, rank, lp };
+		}
 	}
 }
 
