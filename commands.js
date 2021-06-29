a69e3ac089eb26f56a687973cf0c2a3e7b4bd52f
fqhd <fahd31256@gmail.com>
2021-06-29 11:54:14 +0100

    Move updateLeaderboard function to separate file

--- commands.js
+++ commands.js
@@ -1,17 +1,39 @@
+const updateLeaderboard = require("./leaderboard-update");
+let client;
 
+const commands = { 
+	update: updateLeaderboard,
+	stop: function (_client){
+		console.log("Stopping Bot...");
+		_client.destroy();
+	}
+ };
 
-module.exports = async function (message){
+async function messageEntered (message){
 	if(message.channel.id == '831148754181816351'){ // Ranking Leaderboard channel
 		// We delete the message to keep the channel clean
-		console.log('Got a message, deleting it...');
+		console.log('Got a message in leaderboard channel, deleting it...');
 		await message.delete();
 	} else if(message.channel.id == '844341451344314398'){ // Bot Commands Channel
 		// Process the message
 		const tokens = message.content.split(' ');
-		const command = tokens.shift();
+		let command = tokens.shift();
 		if(command.charAt(0) === '_'){
-			command.substring(1);
+			command = command.substring(1);
+			console.log('Command: ' + command);
 			console.log('Tokens: '+ tokens);
+
+			try{
+				commands[command](client);
+			}catch(err){
+				console.log('User entered invalid command');
+			}
 		}
 	}
 }
+
+function initGlobals(_client){
+	client = _client;
+}
+
+module.exports = { initGlobals, messageEntered };
